b315e6241d64e9f4ca5c0011fa2fe4c8d3cac3f3
Sumantamangg <sumantamang1020@gmail.com>
2018-05-06 13:41:32 +0545

    trial version

---
+++ app/src/main/java/com/suman/appointment/TempNavActivity.java
@@ -0,0 +1,13 @@
+package com.suman.appointment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TempNavActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp_nav);
+    }
+}
